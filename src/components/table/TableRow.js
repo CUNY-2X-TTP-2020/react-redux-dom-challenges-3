5ef1dedb6b5bc4f226495fefd564f3ed38ca7e39
RickyC0626 <rickychon99@gmail.com>
2020-06-06 15:19:29 -0400

    Add props to TableRow

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * A presentational component which is rendered by Table
@@ -13,5 +14,14 @@ export default class TableRow extends Component
     constructor(props)
     {
         super(props);
+        this.state =
+        {
+            cells: props.cells
+        }
     }
+}
+
+TableRow.propTypes =
+{
+    cells: PropTypes.array.isRequired
 }
