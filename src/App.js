def6778c1ef562f5ee32602cba9b67c00a0eb2ee
RickyC0626 <rickychon99@gmail.com>
2020-06-06 01:55:27 -0400

    Add project title to app frontend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App()
     return (
         <div className="App">
             <header className="App-header">
-                
+                <h1>DOM Challenges 3</h1>
             </header>
         </div>
     );
