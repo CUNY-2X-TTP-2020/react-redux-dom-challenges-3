d91a583f559120c379edba98439637891dd8cf3c
RickyC0626 <rickychon99@gmail.com>
2020-06-06 12:24:55 -0400

    Add additional information TableRow documentation

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
  * A presentational component which is rendered by Table
  * 
  * The TableRow component will:
+ * - receive props from Table
  * - be a parent of TableCell
  */
 export default class TableRow extends Component
