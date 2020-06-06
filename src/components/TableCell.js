5ed69afaea4220cc3c0b5abeecd4c1cbfe60a785
RickyC0626 <rickychon99@gmail.com>
2020-06-06 12:44:11 -0400

    Add template constructor and render functions to TableCell

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -9,5 +9,16 @@ import React, { Component } from 'react';
  */
 export default class TableCell extends Component
 {
+    constructor(props)
+    {
+        super(props);
+    }
 
+    render()
+    {
+        return (
+            <>
+            </>
+        );
+    }
 }
