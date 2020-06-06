7db400c31b932c6e50009f45c6ed4a6ad1b10bf6
RickyC0626 <rickychon99@gmail.com>
2020-06-06 12:20:28 -0400

    Add documentation and planning for Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 /**
+ * A presentational component which is rendered by App
  * 
+ * The Table component will:
+ * - be a parent of TableRow
+ * - receive bound event handling functions from App to handle the 
+ * creation of rows and columns
  */
 export default class Table extends Component
 {
