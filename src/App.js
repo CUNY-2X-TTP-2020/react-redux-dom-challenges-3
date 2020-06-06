983f291aaa5b333e32f5c4ea635a1927ee496579
RickyC0626 <rickychon99@gmail.com>
2020-06-06 11:57:55 -0400

    Add documentation and planning for App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/**
+ * A stateful class component which handles the rendering of these presentational components:
+ * - Table.js
+ * - TableRow.js
+ * - TableCell.js
+ * 
+ * The minimum viable product features of this app are:
+ * - A user can add a row
+ * - A user can add a column
+ * - A user can select a color
+ * - A user can color in a single cell via click
+ * 
+ * This component will render a single Table component
+ */
 export default class App extends Component
 {
     render()
