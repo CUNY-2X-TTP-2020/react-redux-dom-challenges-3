3efae06f2080e53d4e3332af402071d086a793b3
RickyC0626 <rickychon99@gmail.com>
2020-06-06 12:24:05 -0400

    Add documentation and planning for TableCell component

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 /**
+ * A presentational component which is rendered by TableRow
  * 
+ * The TableCell component will:
+ * - receive props from TableRow
+ * - handle the state of itself (coloring, being clicked)
  */
 export default class TableCell extends Component
 {
