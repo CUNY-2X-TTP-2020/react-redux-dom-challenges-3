2056357c9d9e1a98339db0a828db655cb1efcc58
RickyC0626 <rickychon99@gmail.com>
2020-06-06 13:18:14 -0400

    Add TableCell props and click handler

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * A presentational component which is rendered by TableRow
@@ -12,13 +13,23 @@ export default class TableCell extends Component
     constructor(props)
     {
         super(props);
+        this.state =
+        {
+            color: props.color
+        }
+        this.handleClick = props.clickHandler.bind(this.handleClick);
     }
 
     render()
     {
         return (
-            <>
-            </>
+            <td bgcolor={this.state.color} onClick={this.handleClick} />
         );
     }
+}
+
+TableCell.propTypes =
+{
+    color: PropTypes.string,
+    clickHandler: PropTypes.func
 }
