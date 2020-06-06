7bac36e36108370b530aa4711a202630bb964fd5
RickyC0626 <rickychon99@gmail.com>
2020-06-06 14:31:53 -0400

    Update documentatin of Table component usage

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 /**
- * A presentational component which is rendered by App
+ * A presentational component which is rendered by ControlPanel
  * 
  * The Table component will:
  * - be a parent of TableRow
- * - receive bound event handling functions from App to handle the 
+ * - receive bound event handling functions from ControlPanel to handle the 
  * creation of rows and columns
  */
 export default class Table extends Component
