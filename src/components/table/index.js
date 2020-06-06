4e46cdcba9a63caaa4483e5dee83b4cdef15e760
RickyC0626 <rickychon99@gmail.com>
2020-06-06 13:57:28 -0400

    Moved table related components to separate directory

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,4 +1,4 @@
-// Barrel file for container components, which will be used to compose our main App component
+// Barrel file for Table components, which will be used to compose our main App component
 export { default as Table } from './Table';
 export { default as TableRow } from './TableRow';
 export { default as TableCell } from './TableCell';
