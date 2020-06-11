d3396eac62d279ef9f842c7bcc0fbf5d6205f05b
RickyC0626 <rickychon99@gmail.com>
2020-06-11 03:05:03 -0400

    Add barrel file for Table components

---
+++ src/view/Table/index.js
@@ -0,0 +1,6 @@
+// Barrel file to export all Table components
+
+export { TableContainer } from "./TableContainer";
+export { TableComponent } from "./TableComponent";
+export { TableRowComponent } from "./TableRowComponent";
+export { TableCellComponent } from "./TableCellComponent";
