ec23b42c7a661186b78f0d7749091fa0dd057b8a
RickyC0626 <rickychon99@gmail.com>
2020-06-11 02:03:50 -0400

    Add TableRow presentational component

---
+++ src/view/Table/TableRowComponent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**
+ * A presentational component which renders a single table row.
+ * Each row contains X amount of cells
+ * 
+ * The props it receives are:
+ * - color
+ * - array of cells to render
+ */
+const TableRowComponent = (props) =>
+{
+    return (
+        <tr>
+            {props.cells}
+        </tr>
+    );
+}
+
+export default TableRowComponent;
