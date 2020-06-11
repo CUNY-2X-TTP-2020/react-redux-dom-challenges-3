361f3786ee29eeb4af1ca0acec7d9ca7548a211e
RickyC0626 <rickychon99@gmail.com>
2020-06-10 20:24:20 -0400

    Setup template TableContainer smart component

---
+++ src/view/Table/TableContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+// Smart container
+class TableContainer extends Component
+{
+    render()
+    {
+        return (
+            <section>
+                
+            </section>
+        );
+    }
+}
+
+// Export our store-connected component
+export default TableContainer;
