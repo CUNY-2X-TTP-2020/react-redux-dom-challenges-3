6e925659cb5b6e6b8895552036c58acbc8f31dd6
RickyC0626 <rickychon99@gmail.com>
2020-06-11 23:26:30 -0400

    Organize exports for readability

--- src/state/table/action_types.js
+++ src/state/table/action_types.js
@@ -4,4 +4,10 @@ const ADD_ROW = "ADD_ROW";
 const ADD_COLUMN = "ADD_COLUMN";
 const SELECT_COLOR = "SELECT_COLOR";
 
-export default { CREATE_TABLE, ADD_ROW, ADD_COLUMN, SELECT_COLOR }
+export default 
+{ 
+    CREATE_TABLE, 
+    ADD_ROW, 
+    ADD_COLUMN, 
+    SELECT_COLOR
+}
