1f719af72725eca7eee0238055d2478de805f2bc
RickyC0626 <rickychon99@gmail.com>
2020-06-12 01:21:29 -0400

    Update documentation for TableComponent

--- src/view/Table/TableComponent.jsx
+++ src/view/Table/TableComponent.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+/**
+ * A presentational component which renders the entirety of the table,
+ * consisting of rows and columns
+ * 
+ * The props it receives are:
+ * - rows: array of rows, each containing their own array of individual cells,
+ * which forms the entire table
+ */
 const TableComponent = (props) =>
 {
+    console.log(props);
     return (
         <section>
             <table id="grid">
